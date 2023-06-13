a08cc74ae49108a16b540355a6a062f5d8a7b692
yaneldesigns <85355011+yaneldesigns@users.noreply.github.com>
2023-06-13 11:30:01 -0500

    Update classes.js

--- classes.js
+++ classes.js
@@ -1,1 +1,21 @@
-
+// create classes here
+class Cloth {
+    useDress (){
+        console.log("Wear pink dress")
+    }
+}
+class Casual extends Cloth {
+    useDress (){
+        super.useDress()
+        console.log("Wear purple dress")
+    }
+}
+class Fancy extends Cloth{
+    useDress(){
+        console.log("Wear a black dress")
+    }
+}
+var dressCasual = new Casual();
+var dressFancy = new Fancy();
+dressCasual.useDress();
+dressFancy.useDress();
